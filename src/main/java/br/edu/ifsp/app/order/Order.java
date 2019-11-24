f6fd6a8b7fa94099e2d2f63441aa4c9776d4fef9
Pedro Fonseca <reisensama2345@gmail.com>
2019-11-24 17:56:35 -0300

    Order

---
+++ src/main/java/br/edu/ifsp/app/order/Order.java
@@ -0,0 +1,55 @@
+package br.edu.ifsp.app.order;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity
+@Table(name="order")
+public class Order implements Serializable {	
+
+	private static final long serialVersionUID = 1L;
+
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id
+	private long id;
+	private Integer id_user;
+	private Double total;
+	
+	public Order() {}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Integer getId_user() {
+		return id_user;
+	}
+
+	public void setId_user(Integer id_user) {
+		this.id_user = id_user;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
+	
+	
+}
